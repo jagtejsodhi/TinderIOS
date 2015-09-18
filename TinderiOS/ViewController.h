709866455fefc6407ff94dccd23b16e59fe6c2a4
jagtej <jagtej@yahoo-inc.com>
2015-09-18 11:36:56 -0700

    Initial Commit

---
+++ TinderiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TinderiOS
+//
+//  Created by Jagtej Sodhi on 9/18/15.
+//  Copyright © 2015 Jagtej Sodhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TinderiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TinderiOS
+//
+//  Created by Jagtej Sodhi on 9/18/15.
+//  Copyright © 2015 Jagtej Sodhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
