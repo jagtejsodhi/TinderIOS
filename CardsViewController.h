3777fbc3d5c5cb2d9d717f7c1409b521414738fd
jagtej <jagtej@yahoo-inc.com>
2015-09-18 14:58:33 -0700

    first commit

---
+++ CardsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardsViewController.h
+//  TinderiOS
+//
+//  Created by Jagtej Sodhi on 9/18/15.
+//  Copyright © 2015 Jagtej Sodhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardsViewController : UIViewController
+
+@end
